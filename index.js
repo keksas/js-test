8f863d9039c385d68b7087454ba4c8ffacf2e5b1
keksas <k.rimantas@gmail.com>
2022-04-19 21:38:27 +0300

    Lots of cool stuff

---
+++ index.js
@@ -0,0 +1,53 @@
+const people = [
+  {
+    name: "Petras",
+    color: "blue",
+  },
+  {
+    name: "Petras",
+    color: "red",
+  },
+  {
+    name: "Petras",
+    color: "green",
+  },
+  {
+    name: "Petras",
+    color: "blue",
+  },
+  {
+    name: "Petras",
+    color: "green",
+  },
+  {
+    name: "Petras",
+    color: "orange",
+  },
+];
+
+// atsirenkame kokiu spalvu ir kokiais kiekiais yra masyve
+const colorsCount = {};
+for (const person of people) {
+  const { color } = person;
+  colorsCount[color] = colorsCount[color] ? colorsCount[color] + 1 : 1;
+}
+
+// randame dazniausiai paminetos spalvos kieki (spalvos pavadinimas nera aktualus)
+let mostPopularColorCount = 0;
+for (const color in colorsCount) {
+  const count = colorsCount[color];
+  if (count > mostPopularColorCount) {
+    mostPopularColorCount = count;
+  }
+}
+
+// randame visus spalvu pavadinimus, kurie atitinka didziausia paminejimu kieki
+const mostPopularColorNames = [];
+for (const color in colorsCount) {
+  const count = colorsCount[color];
+  if (count === mostPopularColorCount) {
+    mostPopularColorNames.push(color);
+  }
+}
+
+console.log(mostPopularColorNames);
---
+++ math.js
@@ -0,0 +1,14 @@
+const math = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "/": (a, b) => a / b,
+  "*": (a, b) => a * b,
+};
+
+const action = "-";
+const n1 = 7;
+const n2 = 5;
+
+const ans = math[action](n1, n2);
+
+console.log(`${n1} ${action} ${n2} = ${ans}`);
